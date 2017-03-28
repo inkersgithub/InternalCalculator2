265cd47d3472e81aa850c79baa21a63e266a2760
inkersgithub <inkers.info@gmail.com>
2017-03-28 23:09:08 +0530

    added about page and ktu deleted

--- app/src/main/java/in/inkers/internalcalculator/IntroActivity.java
+++ app/src/main/java/in/inkers/internalcalculator/IntroActivity.java
@@ -2,9 +2,14 @@ package in.inkers.internalcalculator;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by zade on 21/2/17.
@@ -12,27 +17,49 @@ import android.widget.Spinner;
 
 public class IntroActivity extends AppCompatActivity {
 
-    Spinner spinneruse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_activity);
     }
-    public void onOkClick(View view) {
-        spinneruse = (Spinner) findViewById(R.id.spinner);
-        String item = spinneruse.getSelectedItem().toString();
-        if (item.equals("B.Tech(Calicut University 2014 scheme)")){
+    public void onGoClick(View view) {
             Intent intent=new Intent(this,CalicutActivity.class);
             startActivity(intent);
-        }
-        else if(item.equals("B.Tech(KTU)")){
-            Intent intent=new Intent(getApplicationContext(),KtuActivity.class);
-            startActivity(intent);
-        }
-        else if(item.equals("MBBS(KUHS)")){
-            Intent intent=new Intent(getApplicationContext(),MbbsActivity.class);
-            startActivity(intent);
-        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        AlertDialog.Builder dialog = new AlertDialog.Builder(IntroActivity.this);
+        View dView = getLayoutInflater().inflate(R.layout.activity_dialog, null);
+        TextView dtv = (TextView) dView.findViewById(R.id.dtv);
+        TextView smtv = (TextView) dView.findViewById(R.id.smtv);
+        TextView sa1bal = (TextView) dView.findViewById(R.id.se1baltv);
+        TextView sa2bal = (TextView) dView.findViewById(R.id.se2baltv);
+        TextView as1bal = (TextView) dView.findViewById(R.id.as1baltv);
+        TextView as2bal = (TextView) dView.findViewById(R.id.as2baltv);
+        dtv.setText("Developed by:");
+        smtv.setText("inkers Inc.");
+        sa1bal.setText("www.inkers.in");
+        sa2bal.setVisibility(View.GONE);
+        as1bal.setVisibility(View.GONE);
+        as2bal.setVisibility(View.GONE);
+        Button btok = (Button) dView.findViewById(R.id.bt2);
+        dialog.setView(dView);
+        final AlertDialog adialog = dialog.create();
+        adialog.show();
+        btok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adialog.dismiss();
+            }
+        });
+        return true;
     }
 }
 
